c006be4b8cf19533fe56dabff9c90b43a4bcd1df
daksh123456 <58256644+daksh123456@users.noreply.github.com>
2024-02-08 18:18:53 +0530

    index.js

--- index.js
+++ index.js
@@ -3,6 +3,46 @@ var rightSide = document.querySelector('.right-side');
 var postBtn = document.getElementById('post-btn');
 var textCont = document.querySelector('.text-area');
 var rightSideCont = document.querySelector('.right-side-container');
+var data = {
+    tweets : [
+
+    ]
+}
+
+var parsedData = JSON.parse(localStorage.getItem('TweetData'));
+if(parsedData && parsedData.tweets.length > 0){
+    data.tweets = data.tweets.concat(parsedData.tweets);
+}
+
+function setToStorage(){
+    localStorage.setItem('TweetData', JSON.stringify(data));
+}
+
+showTickets();
+function showTickets(){
+    for(var i = 0; i < data.tweets.length; i++){
+        var tweet = document.createElement('div');
+        var navbar = document.createElement('div');
+        var profilepic = document.createElement('img');
+        var tweetText = document.createElement('text');
+        tweetText.classList.add('tweet-text');
+        tweetText.innerText = data.tweets[i];
+
+        navbar.classList.add('navbar2');
+        var name = document.createElement('div');
+
+        var icon = document.createElement('div');
+        var icon1 = document.createElement('img');
+        icon1.src = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/030/original/delete-color-filled.png?1706888714";
+        var icon2 = document.createElement('img');
+        icon2.src = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/028/original/edit.png?1706888661";
+
+        tweet.classList.add('tweet-cont');
+        tweet.appendChild(tweetText);
+        rightSideCont.appendChild(tweet);
+    }
+
+}
 
 textCont.addEventListener('click', function(){
     let text = textCont.innerText;
@@ -20,6 +60,7 @@ postBtn.addEventListener('click', function(){
     }
     textCont.innerText = "Type your thoughts here....";
 })
+
 function createTweet(){
     let text = textCont.innerText;
     var tweet = document.createElement('div');
@@ -39,6 +80,8 @@ function createTweet(){
     icon2.src = "https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/064/028/original/edit.png?1706888661";
 
     tweet.classList.add('tweet-cont');
+    data.tweets.push(text);
+    setToStorage();
 
     tweet.appendChild(tweetText);
     rightSideCont.appendChild(tweet);
